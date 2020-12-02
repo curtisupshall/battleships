041a8017938961d00be29b1c636e98e8d6acd344
Curtis Upshall <curtisupshall@gmail.com>
2020-12-02 13:26:39 -0800

    Finish splash page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
-import GameBoard from '../components/GameBoard'
+import {
+    Button
+} from '@material-ui/core'
 
 
 const HomePage = () => (
     <div id='homepage'>
-        <h1>Hello world!</h1>
-        <GameBoard />
+        <div className='background-image' />
+        <div className='homepage__content'>
+            <h1>Play Battleships Online!</h1>
+            <Link href='games'>
+                <a>
+                    <Button variant='contained' color='primary'>Play Now</Button>
+                </a>
+            </Link>
+        </div>
     </div>
 )
+
 
 export default HomePage
