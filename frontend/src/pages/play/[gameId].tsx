14afc8dd70da28f16c6f009ee65ece860e79baf9
Curtis Upshall <curtisupshall@gmail.com>
2020-12-03 17:37:05 -0800

    Stylize frontend

--- frontend/src/pages/play/[gameId].tsx
+++ frontend/src/pages/play/[gameId].tsx
@@ -1,6 +1,7 @@
+import classNames from 'classnames'
 import React from 'react'
 
-import { IGameState } from '../../../../api/src/types'
+import { Typography, Paper } from '@material-ui/core'
 
 import Flexbox from '../../components/Flexbox'
 import GameBoard from '../../components/GameBoard'
@@ -9,13 +10,25 @@ import withAuth from '../../hocs/withAuth'
 
 const PlayPage = () => {
     // const game: IGame = {}
+    const playStatus: string = ''
 
     return (
         <div id='play'>
-            <Flexbox flexDirection='column' alignItems='center'>
-                <GameBoard />
-                <GameBoard />
+            <Flexbox flexDirection='row' justifyContent='center'>
+                <Flexbox flexDirection='column' alignItems='center'>
+                    <GameBoard />
+                    <GameBoard />
+                </Flexbox>
+                <Paper className='play__stats'>
+                    <Typography variant='h5'>You</Typography>
+                    <Typography variant='subtitle1'>v.s.</Typography>
+                    <Typography variant='h5'>WholesomePoggers100</Typography>
+                    <Typography variant='h6' className={classNames('play__status', `--${playStatus}`)}>
+                        It's your turn.
+                    </Typography>
+                </Paper>
             </Flexbox>
+            
         </div>
     )
 }
